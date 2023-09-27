1ce1005b3963122b10fdbc8cc3add04f22f44eea
RobCastillo11 <rob.kztillo117@gmail.com>
2023-09-26 21:08:49 -0600

    new file:   Bob/PS/js/consulta.js 	new file:   Bob/PS/php/consultar.php 	new file:   Bob/PS/php/modificacion.php 	new file:   Bob/PS/php/modificar.php 	modified:   Bob/PS/php/pag1.php

---
+++ Bob/PS/php/consultar.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostrar Datos de Empleado</title>
+</head>
+<body>
+    <h1>Mostrar Datos de Empleado</h1>
+
+    <?php
+    require 'database.php';
+
+    if ($conn->connect_error) {
+        die("La conexión falló: " . $conn->connect_error);
+    }
+
+    $noEmpleado = $_GET['NO_EMPLEADO'];
+
+    $sql = "SELECT * FROM generales WHERE NO_EMPLEADO = '$noEmpleado'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+    ?>
+
+    <form action="mostrar_datos.php" method="GET">
+        <!-- Campo de texto para el NO_EMPLEADO -->
+        <label for="noEmpleado">Número de Empleado:</label>
+        <input type="text" id="noEmpleado" name="NO_EMPLEADO" required readonly value="<?php echo $row['NO_EMPLEADO']; ?>">
+        <br><br>
+
+        <!-- Campo de texto para el NOMBRE -->
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="NOMBRE" required readonly value="<?php echo $row['NOMBRE']; ?>">
+        <br><br>
+
+        <!-- Campo de texto para el AP -->
+        <label for="ap">Apellido Paterno:</label>
+        <input type="text" id="ap" name="AP" required readonly value="<?php echo $row['AP']; ?>">
+        <br><br>
+
+        <!-- Campo de texto para el FN -->
+        <label for="fn">Fecha de Nacimiento:</label>
+        <input type="text" id="fn" name="FN" required readonly value="<?php echo $row['FN']; ?>">
+        <br><br>
+
+        <!-- Botón de envío -->
+        <input type="submit" value="Mostrar Datos">
+    </form>
+
+    <!-- Mostrar los datos recibidos del formulario -->
+    <h2>Datos del Empleado</h2>
+    <p>NO_EMPLEADO: <span id="mostrarNoEmpleado"><?php echo $row['NO_EMPLEADO']; ?></span></p>
+    <p>NOMBRE: <span id="mostrarNombre"><?php echo $row['NOMBRE']; ?></span></p>
+    <p>AP: <span id="mostrarAP"><?php echo $row['AP']; ?></span></p>
+    <p>FN: <span id="mostrarFN"><?php echo $row['FN']; ?></span></p>
+
+    <script>
+        // JavaScript para mostrar los datos recibidos en el formulario
+        const formulario = document.querySelector('form');
+        const noEmpleadoInput = document.getElementById('noEmpleado');
+        const nombreInput = document.getElementById('nombre');
+        const apInput = document.getElementById('ap');
+        const fnInput = document.getElementById('fn');
+        const mostrarNoEmpleado = document.getElementById('mostrarNoEmpleado');
+        const mostrarNombre = document.getElementById('mostrarNombre');
+        const mostrarAP = document.getElementById('mostrarAP');
+        const mostrarFN = document.getElementById('mostrarFN');
+
+        formulario.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            // Mostrar los datos recibidos en el formulario
+            mostrarNoEmpleado.textContent = noEmpleadoInput.value;
+            mostrarNombre.textContent = nombreInput.value;
+            mostrarAP.textContent = apInput.value;
+            mostrarFN.textContent = fnInput.value;
+        });
+    </script>
+
+    <?php
+    } else {
+        echo "<p>Empleado no encontrado.</p>";
+    }
+
+    $conn->close();
+    ?>
+</body>
+</html>
--- Bob/PS/php/pag1.php
+++ Bob/PS/php/pag1.php
@@ -133,109 +133,6 @@ session_start(); // Inicia la sesión
               </div>
               <div class="col-6" id="nav-paginacion"></div>
             </div>
-
-            <script>
-              let paginaActual = 1;
-              getData(paginaActual);
-
-              document.getElementById("campo").addEventListener("keyup", function () {
-                getData(1)
-              }), false;
-
-              document.getElementById("num_registros").addEventListener("change", function () {
-                getData(paginaActual)
-              }), false;
-
-              function getData(pagina) {
-                let input = document.getElementById("campo").value
-                let num_registros = document.getElementById("num_registros").value
-                let content = document.getElementById("content")
-                let lblTotal = document.getElementById("lbl-total")
-                let navPaginacion = document.getElementById("nav-paginacion")
-
-                if(pagina != null){
-                  paginaActual = pagina
-                }
-
-                let url = "consulta_empleado.php"
-                let formData = new FormData()
-                formData.append('campo', input)
-                formData.append('registros', num_registros)
-                formData.append('pagina', paginaActual)
-
-                // Limpiar la tabla antes de agregar nuevos datos
-                content.innerHTML = '';
-
-                fetch(url, {
-                  method: "POST",
-                  body: formData
-                }).then(Response => Response.json())
-                  .then(data => {
-                    if (data.data.length > 0) {
-                      data.data.forEach(rowData => {
-                        // Crear una nueva fila
-                        let row = document.createElement("tr");
-
-                        // Agregar celdas con los datos
-                        let noEmpleadoCell = document.createElement("td");
-                        noEmpleadoCell.textContent = rowData.NO_EMPLEADO;
-                        row.appendChild(noEmpleadoCell);
-
-                        let nombreCell = document.createElement("td");
-                        nombreCell.textContent = rowData.NOMBRE;
-                        row.appendChild(nombreCell);
-
-                        let apellidoCell = document.createElement("td");
-                        apellidoCell.textContent = rowData.AP;
-                        row.appendChild(apellidoCell);
-
-                        let fnCell = document.createElement("td");
-                        fnCell.textContent = rowData.FN;
-                        row.appendChild(fnCell);
-
-                        let fechaRegistroCell = document.createElement("td");
-                        fechaRegistroCell.textContent = rowData.fecha_registro;
-                        row.appendChild(fechaRegistroCell);
-
-                        // Agregar botón de "Modificar"
-                        let modificarCell = document.createElement("td");
-                        let modificarButton = document.createElement("button");
-                        modificarButton.textContent = "Modificar";
-                        modificarCell.appendChild(modificarButton);
-                        row.appendChild(modificarCell);
-
-                        // Agregar botón de "Consultar"
-                        let consultarCell = document.createElement("td");
-                        let consultarButton = document.createElement("button");
-                        consultarButton.textContent = "Consultar";
-                        consultarCell.appendChild(consultarButton);
-                        row.appendChild(consultarCell);
-
-                        // Agregar la fila a la tabla
-                        content.appendChild(row);
-                      });
-
-                      // Mostrar el número de registros y la paginación
-                      lblTotal.textContent = 'Mostrando ' + data.totalFiltro +
-                        ' de ' + data.totalRegistros + ' registros ';
-                      navPaginacion.innerHTML = data.paginacion;
-                    } else {
-                      // Si no hay resultados, mostrar un mensaje
-                      let noResultsRow = document.createElement("tr");
-                      let noResultsCell = document.createElement("td");
-                      noResultsCell.colSpan = 7;
-                      noResultsCell.textContent = "Sin resultados";
-                      noResultsRow.appendChild(noResultsCell);
-                      content.appendChild(noResultsRow);
-                      lblTotal.textContent = ''; // Ocultar el mensaje de registros
-                      navPaginacion.innerHTML = ''; // Ocultar la paginación
-                    }
-
-                    // Actualizar la paginación aquí si es necesario
-
-                  }).catch(err => console.log(err));
-              }
-            </script>
             </div>
         </div>
     
@@ -247,6 +144,7 @@ session_start(); // Inicia la sesión
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="../js/consulta.js"></script>
   <script src="../js/pag1.js"></script>
 </body>
 </html>
